fba05bd1495c5bd44613d89d9c9627ae65d7180e
Swasti Aggarwal <swastiaggarwal2501@gmail.com>
2025-08-13 00:46:20 +0530

    Print B

---
+++ ALPHABETS/letter2.java
@@ -0,0 +1,26 @@
+public class letter2 
+{
+       public static void main(String[] args)
+        {
+            for(int i=0; i<7; i++)
+            {
+                for(int j=0; j<=7/2; j++)
+                {
+                    if (j == 0 || 
+                   (i == 0 || i == 7 / 2 || i == 7 - 1) && j != 7 / 2 || 
+                   (j == 7 / 2 && i != 0 && i != 7 / 2 && i != 7 - 1))
+                    {
+                        System.out.print(" * ");
+                    }
+                    else
+                    {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+        
+    }
+    
+    
