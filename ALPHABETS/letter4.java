fd3e3652ff6411ea7f8a8f04f5d8a756b67b7294
Swasti Aggarwal <swastiaggarwal2501@gmail.com>
2025-08-15 00:48:40 +0530

    Print D

---
+++ ALPHABETS/letter4.java
@@ -0,0 +1,21 @@
+public class letter4 {
+    public static void main(String[] args)
+    {
+        for(int i=0; i<7; i++)
+        {
+            for(int j=0; j<=7/2; j++)
+            {
+                if(j==0 || i == 0 || j == 7/2 || i == 6)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
