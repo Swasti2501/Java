4ba0916f4dd2069910b0d869cee762e43cc048a0
Swasti Aggarwal <swastiaggarwal2501@gmail.com>
2025-08-27 00:22:15 +0530

    Print P

---
+++ ALPHABETS/letter16.java
@@ -0,0 +1,21 @@
+public class letter16
+{
+    public static void main(String[] args)
+    {
+        for(int i=0; i<7; i++)
+        {
+            for(int j=0; j<=7/2; j++)
+            {
+                if((j == 0) || (i == 0) || (i == 3) || (j == 7/2 && i < 3))
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }    
+}
