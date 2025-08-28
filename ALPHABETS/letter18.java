640194ed53c8f193d92c42d7ea4580af60fbe2d5
Swasti Aggarwal <swastiaggarwal2501@gmail.com>
2025-08-29 00:20:31 +0530

    Print R

---
+++ ALPHABETS/letter18.java
@@ -0,0 +1,15 @@
+public class letter18 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j <= 7 / 2; j++) {
+                if ((j == 0) || (i == 0) || (j == 7 / 2 && i < 3) || (i == 3 && j != 0) || (i - j == 7 / 2))
+                {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
