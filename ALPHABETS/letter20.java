96ebcf023e40b7583a1bdb23c903fcccaf99170a
Swasti Aggarwal <swastiaggarwal2501@gmail.com>
2025-08-31 09:52:27 +0530

    Print T

---
+++ ALPHABETS/letter20.java
@@ -0,0 +1,22 @@
+public class letter20
+{
+    public static void main(String[] args)
+    {
+        int n=8;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<=n/2; j++)
+            {
+                if((i==0) || (j==n/4))
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }   
+}
