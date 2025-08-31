f1b6d37c9970c11d49dfd882a705bb1d62654cc9
Swasti Aggarwal <swastiaggarwal2501@gmail.com>
2025-09-01 02:11:53 +0530

    Print U

---
+++ ALPHABETS/letter21.java
@@ -0,0 +1,22 @@
+public class letter21
+{
+    public static void main(String[] args)
+    {
+        int n=6;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<=n/2; j++)
+            {
+                if((j==0) || (j==n/2) || (i==n-1))
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }   
+}
