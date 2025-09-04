f0175bafdc157acd3cd41cfa9d50868cd99a2b87
Swasti Aggarwal <swastiaggarwal2501@gmail.com>
2025-09-05 01:38:38 +0530

    Print Y

---
+++ ALPHABETS/letter25.java
@@ -0,0 +1,22 @@
+public class letter25
+{
+    public static void main(String[] args)
+    {
+        int n=7;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<=n; j++)
+            {
+                if(i+j == n || (i==j && i<=n/2))
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }    
+}
