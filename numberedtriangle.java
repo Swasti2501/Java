38daea43bdddd84f245d393e40ec978877f7f81f
Swasti Aggarwal <swastiaggarwal2501@gmail.com>
2025-09-10 16:57:06 +0530

    Number Triangle

---
+++ numberedtriangle.java
@@ -0,0 +1,14 @@
+public class numberedtriangle 
+{
+   public static void main(String[] args)
+   {
+    for(int i=0; i<=5; i++)
+    {
+        for(int j=0; j<=i; j++)
+        {
+            System.out.print( i+1 );
+        }
+        System.out.println();
+    }
+   }  
+}
