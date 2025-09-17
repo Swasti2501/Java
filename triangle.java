38d756ad1ee2b2fc73d5f46cad1fab4d1a4da4ef
Swasti Aggarwal <swastiaggarwal2501@gmail.com>
2025-09-17 12:14:49 +0530

    Triangle

---
+++ triangle.java
@@ -0,0 +1,23 @@
+public class triangle
+{
+    public static void main(String[] args)
+    {
+        int rows = 5;
+        for (int i=0;i<=rows;i++)
+        {
+            for (int j=1; j<=i; j++)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        for(int i = rows - 1; i>=1; i--)
+        {
+            for(int j = 1; j<= i; j++)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
