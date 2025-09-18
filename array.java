114e603399cd9ce7d6ab4bd4de5fc2be914aa776
Swasti Aggarwal <swastiaggarwal2501@gmail.com>
2025-09-18 20:00:11 +0530

    Print an array of first 5 natural numbers

---
+++ array.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class array {
+    public static void main(String[] args)
+    {
+        int[] array = {1,2,3,4,5};
+        System.out.print(Arrays.toString(array));
+    }
+    
+}
